01a1d7c5dc24efab4647a273c2430c097fd589e7
3mirSahin <72044701+3mirSahin@users.noreply.github.com>
2020-11-02 23:06:10 +0000

    Update VideoExtractor.py
    Got rid of the b - wasn't being used.
    
    Changed the input location to annotated. The general code got rid of the annotated branch. It was added into the function.
    
    Adjusted the output location of the video to inside the V* files.

--- VideoExtractor.py
+++ VideoExtractor.py
@@ -7,20 +7,20 @@ def makeVideo(video):
     img_array = []
 
     #make a list of files:
-    frames = os.listdir(video)
+    frames = os.listdir(video + "annotated/")
     frames = len(frames) #number of frames in the video
 
     #add frames into the img array
     for i in range(frames+1):
         i = str(i)
         n = 5-len(i)
-        fin = video + "I"+  n*"0" + i +".jpg" #might require more adjusting depending on how the names are structured but seems to work for now
+        fin = video + "annotated/I"+  n*"0" + i +".jpg" #might require more adjusting depending on how the names are structured but seems to work for now
         print(fin)
         img_array.append(cv2.imread(fin))
 
     height,width,layers = img_array[1].shape
 
-    result = cv2.VideoWriter(video+"video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 15, (width, height))
+    result = cv2.VideoWriter(locale+"video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 15, (width, height))
 
     for i in range(len(img_array)):
         result.write(img_array[i])
@@ -33,10 +33,8 @@ sets = glob.glob('Sets/set*')
 #print(sets)
 videos = {}
 for set in sets:
-    videos[set] = glob.glob(str(set) + "/V*/lwir/" ) #don't forget to adjust lwir here
-
-b = False
-
+    videos[set] = glob.glob(str(set) + "/V*/" ) #moved annotated into the function
+    
 for key in videos:
     for video in videos[key]:
         makeVideo(video)
