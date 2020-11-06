db5bf91f84811c1a4904fe178cec39d202b2801f
3mirSahin <72044701+3mirSahin@users.noreply.github.com>
2020-11-06 18:44:49 +0000

    Update VideoExtractor.py

--- VideoExtractor.py
+++ VideoExtractor.py
@@ -14,13 +14,13 @@ def makeVideo(video):
     for i in range(frames+1):
         i = str(i)
         n = 5-len(i)
-        fin = video + "annotated/I"+  n*"0" + i +".jpg" #might require more adjusting depending on how the names are structured but seems to work for now
+        fin = video + "annotated/I"+  n*"0" + i +"_bounded.jpg" #might require more adjusting depending on how the names are structured but seems to work for now
         print(fin)
         img_array.append(cv2.imread(fin))
 
     height,width,layers = img_array[1].shape
 
-    result = cv2.VideoWriter(locale+"video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 15, (width, height))
+    result = cv2.VideoWriter(video+"video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 15, (width, height))
 
     for i in range(len(img_array)):
         result.write(img_array[i])
