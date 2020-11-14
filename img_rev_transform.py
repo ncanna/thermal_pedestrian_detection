567e39ead0d6281a877faf729368d33dc66d1c5f
Sander <51409310+sanderabraham@users.noreply.github.com>
2020-11-14 15:47:35 -0500

    Update img_rev_transform.py

--- img_rev_transform.py
+++ img_rev_transform.py
@@ -1,10 +1,16 @@
 import os, glob
 import cv2 as cv
 
-cutoutDir = ""
+main_folder = os.path.dirname(os.path.abspath(__file__))
 
-for img in cutoutDir:
-    original_img = cv.imread(img, 1)
-    img_name_rev = os.path.basename(img) + "_rev.jpg"
-    flipped = cv.flip(original_img, 1)
-    cv.imwrite(img_name_rev, flipped)
+train_folder = os.path.join(main_folder, "train")
+
+for obj_dir in train_folder:
+    print(obj_dir)
+    for img in obj_dir:
+        print(img)
+        original_img = cv.imread(img, 1)
+        img_name_rev = os.path.basename(img) + "_rev.jpg"
+        target_name = os.path.join(train_folder, obj_dir) + "/" + img_name_rev
+        flipped = cv.flip(original_img, 1)
+        cv.imwrite(target_name, flipped)
