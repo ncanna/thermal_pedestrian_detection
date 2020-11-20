7c1edad5ce9e072ced8d2d7e16ed643a8d7d4721
Sander <51409310+sanderabraham@users.noreply.github.com>
2020-11-20 13:23:43 -0500

    Update img_rev_transform.py

--- img_rev_transform.py
+++ img_rev_transform.py
@@ -1,6 +1,9 @@
 import os, glob
 import cv2 as cv
 
+#### CODE IS NOT OPERABLE ####
+
+
 main_folder = os.path.dirname(os.path.abspath(__file__))
 
 train_folder = os.path.join(main_folder, "train")
@@ -11,7 +14,17 @@ for obj_dir in os.listdir(train_folder):
     for img in os.listdir(target_folder):
         print(img)
         original_img = cv.imread(img, 1)
+
+        cv.imshow("original", original_img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
         img_name_rev = os.path.basename(img) + "_rev.jpg"
-        target_name = os.path.join(train_folder, obj_dir) + "/" + img_name_rev
+        target_name = target_folder + "/" + img_name_rev
         flipped = cv.flip(original_img, 1)
+
+        cv.imshow(img_name_rev, flipped)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
         cv.imwrite(target_name, flipped)
