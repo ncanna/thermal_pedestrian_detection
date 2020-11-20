c32d789c0a51fc92c173d93a2411487709254620
Sander <51409310+sanderabraham@users.noreply.github.com>
2020-11-20 13:15:36 -0500

    iterating through folder correctly. need to fix image reverse

--- img_rev_transform.py
+++ img_rev_transform.py
@@ -5,9 +5,10 @@ main_folder = os.path.dirname(os.path.abspath(__file__))
 
 train_folder = os.path.join(main_folder, "train")
 
-for obj_dir in train_folder:
+for obj_dir in os.listdir(train_folder):
     print(obj_dir)
-    for img in obj_dir:
+    target_folder = os.path.join(train_folder, obj_dir)
+    for img in os.listdir(target_folder):
         print(img)
         original_img = cv.imread(img, 1)
         img_name_rev = os.path.basename(img) + "_rev.jpg"
