63f41e268b60c49d1b772b85ad4f1aa04788a911
Sander <51409310+sanderabraham@users.noreply.github.com>
2020-12-12 01:53:02 -0500

    Update hpc_pathing.py

--- hpc_pathing.py
+++ hpc_pathing.py
@@ -2,5 +2,6 @@ import os, time
 
 # Need to double check what the main folder is on rivanna
 main_folder = os.path.dirname(os.path.abspath(__file__))
-time.sleep(5)
+# time.sleep(5)
 print(main_folder)
+os.makedirs("/scratch/sa3ag/modelRuns")
