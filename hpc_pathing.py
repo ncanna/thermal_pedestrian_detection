3bcd5fc28e1c8fef60b6cd06f5e9b001d13ac866
Sander <51409310+sanderabraham@users.noreply.github.com>
2020-12-12 01:57:21 -0500

    Update hpc_pathing.py

--- hpc_pathing.py
+++ hpc_pathing.py
@@ -4,4 +4,4 @@ import os, time
 main_folder = os.path.dirname(os.path.abspath(__file__))
 # time.sleep(5)
 print(main_folder)
-os.makedirs("/scratch/sa3ag/modelRuns")
+# os.makedirs("/scratch/sa3ag/modelRuns") #Check I can make a file in scratch from project
