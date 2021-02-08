e6841b73ca100d7f36f11a8dced2ba3a551cf07a
ncanna <na3au@virginia.edu>
2021-02-07 23:56:10 -0500

    Update local behavior, indexing, and IOU functions

--- test_model.py
+++ test_model.py
@@ -39,15 +39,30 @@ elif user == "s":
     xml_ver_string = "xml"
 
 local_mode = True
-selfcsv_df = pd.read_csv("frame_MasterList.csv").head(50)
+
 if local_mode:
     model_string = "full_model_gpu.pt"
-    modelPath = os.getcwd() + "/" +model_string
     batch_size = 10
+    selfcsv_df = pd.read_csv("frame_MasterList.csv").head(50)
+    dir_path = os.getcwd()
+    modelPath = dir_path + "/" +model_string
+
 else:
     model_string = "2021_01_04-08_23_03_PM_NOTEBOOK/full_model_25.pt"
-    modelPath = "/scratch/" + computing_id + "/modelRuns" + "/" + model_string
     batch_size = 32
+    selfcsv_df = pd.read_csv("frame_MasterList.csv")
+    dir_path = "/scratch/" + computing_id + "/modelRuns"
+    modelPath = dir_path + "/" + model_string
+
+try:
+    current_time = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+    directory = dir_path + "/" + current_time + "_TESTING"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    print(f'Creation of directory at {directory} successful')
+except:
+    print(f'Creation of directory at {directory} failed')
+file_output_path = directory + "/"
 
 #req
 def get_box(obj):
@@ -155,7 +170,7 @@ data_loader = torch.utils.data.DataLoader(
     dataset, batch_size=batch_size, collate_fn=collate_fn)
 
 len_dataloader = len(data_loader)
-print(f'Length of test dataset: {len_dataloader}')
+print(f'Length of test dataset: {len_dataloader} ({len_dataloader+1} images)')
 
 def get_model_instance_segmentation(num_classes):
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True)
@@ -190,7 +205,7 @@ preds = model(imgs)
 print(f"{len(preds)} Predictions loaded")
 
 
-def plot_images(num, input):
+def plot_images(num):
     fig, ax = plt.subplots(nrows=1, ncols=2)
     img_tensor = imgs[num]
     annotation = annotations[num]
@@ -246,14 +261,157 @@ def plot_images(num, input):
     # fig.savefig(figname)
     plt.show()
 
+def get_iou(num):
+    annotation = annotations[num]
+    prediction = preds[num]
+
+    annotation_boxes = annotation["boxes"].tolist()
+
+    ix = 0
+    for box in annotation["boxes"]:
+        img_id = annotation["image_id"].item()
+        file_name = selfcsv_df.loc[img_id, :].image_path
+        set = file_name.split("/")[7]
+        video = file_name.split("/")[8]
+        file_name = file_name.split("/")[10]
+        file_name = file_name[:-4]
+        output_name = set + "_" + video + "_" + file_name
+        ix += 1
+
+    ix = 0
+    voc_iou = []
+    #print(f'{len(prediction["boxes"])} prediction boxes made for {len(annotation["boxes"])}
+    # actual boxes in {str(output_name)} for {identifier} with note {input}')
+    for box in prediction["boxes"]:
+        xmin, ymin, xmax, ymax = box.tolist()
+        iou_list = []
+        for bound in annotation_boxes:
+            a_xmin, a_ymin, a_xmax, a_ymax = bound
+            xA = max(xmin, a_xmin)
+            yA = max(ymin, a_ymin)
+            xB = min(xmax, a_xmax)
+            yB = min(ymax, a_ymax)
+            interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+            p_area = (xmax - xmin + 1) * (ymax - ymin + 1)
+            a_area = (a_xmax - a_xmin + 1) * (a_ymax - a_ymin + 1)
+            iou = interArea / float(p_area + a_area - interArea)
+            iou_list.append(iou)
+        max_val = max(iou_list)
+        voc_iou.append(max_val)
+        ix += 1
+
+    if len(voc_iou) == 0:
+        mean_iou = 0
+        print(f'No predictions for Image {num} made so Mean IOU: {mean_iou}')
+    else:
+        mean_iou = sum(voc_iou) / len(voc_iou)
+        print(f'Predictions for Image {num} have Mean IOU: {mean_iou}')
+
+    return [mean_iou, voc_iou]
+
+def plot_iou(num, input="iou_plotted"):
+    fig, ax = plt.subplots(1)
+
+    identifier = "Test"
+    print(identifier)
+    img_tensor = imgs[num]
+    annotation = annotations[num]
+    prediction = preds[num]
+
+    img = img_tensor.cpu().data
+    img = img[0, :, :]
+    annotation_boxes = annotation["boxes"].tolist()
+
+    if local_mode:
+        ax.imshow(img, cmap='gray')
+
+    ix = 0
+    for box in annotation["boxes"]:
+        xmin, ymin, xmax, ymax = box.tolist()
+        value = annotation["labels"][ix]
+        img_id = annotation["image_id"].item()
+        file_name = selfcsv_df.loc[img_id, :].image_path
+        set = file_name.split("/")[7]
+        video = file_name.split("/")[8]
+        file_name = file_name.split("/")[10]
+        file_name = file_name[:-4]
+        output_name = set + "_" + video + "_" + file_name + "_" + identifier
+        text = Recode(value)
+        colors = ["r", "r", "r"]
+        rect = patches.Rectangle((xmin, ymin), (xmax - xmin), (ymax - ymin), linewidth=1,
+                                 edgecolor=colors[value], facecolor='none')
+        target_x = xmin
+        target_y = ymin - 5
+        ax.text(target_x, target_y, text, color=colors[value])
+        ax.add_patch(rect)
+        ix += 1
+
+    ix = 0
+    voc_iou = []
+    print(
+        f'{len(prediction["boxes"])} prediction boxes made for {len(annotation["boxes"])} actual boxes in {str(output_name)} for {identifier} with note {input} (INDEX {num})')
+    for box in prediction["boxes"]:
+        xmin, ymin, xmax, ymax = box.tolist()
+
+        iou_list = []
+        for bound in annotation_boxes:
+            a_xmin, a_ymin, a_xmax, a_ymax = bound
+            xA = max(xmin, a_xmin)
+            yA = max(ymin, a_ymin)
+            xB = min(xmax, a_xmax)
+            yB = min(ymax, a_ymax)
+            interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+            p_area = (xmax - xmin + 1) * (ymax - ymin + 1)
+            a_area = (a_xmax - a_xmin + 1) * (a_ymax - a_ymin + 1)
+            iou = interArea / float(p_area + a_area - interArea)
+            iou_list.append(iou)
+        max_val = max(iou_list)
+        voc_iou.append(max_val)
+
+        max_ix = iou_list.index(max_val)
+        map_dict = {max_ix: max_val}
+
+        # iou_string = ', '.join((str(float) for float in iou_list))
+        value = prediction["labels"][ix]
+        text = json.dumps(map_dict)
+        colors = ["r", "#00FF00", "#0000FF"]
+        rect = patches.Rectangle((xmin, ymin), (xmax - xmin), (ymax - ymin), linewidth=1,
+                                 edgecolor=colors[value], facecolor='none')
+        target_x = xmin
+        target_y = ymin - 5
+        ax.text(target_x, target_y, text, color=colors[value])
+        ax.add_patch(rect)
+        ix += 1
+
+    if local_mode:
+        plt.show()
+
+    if len(voc_iou) == 0:
+        mean_iou = 0
+        print(f'No predictions made so Mean IOU: {mean_iou}')
+    else:
+        mean_iou = sum(voc_iou) / len(voc_iou)
+        fp = voc_iou.count(0) / len(voc_iou) * 100
+        bp = sum((i > 0 and i < 0.5) for i in voc_iou) / len(voc_iou) * 100
+        gp = sum((i >= 0.5) for i in voc_iou) / len(voc_iou) * 100
+        print(f'{fp} false positives (IOU = 0)')
+        print(f'{bp} bad positives (0 < IOU < 0.5)')
+        print(f'{gp} good positives (IOU >= 0.5)')
+        print(f'Mean IOU: {mean_iou}')
+
+    figname = output_name + "_" + input + ".png"
+    fig.savefig(file_output_path + figname)
+    #print(f'Figure {figname} saved to {directory}.')
 
 print("Predicted:")
-for i in range(len(preds) - 1):
+for i in range(len(preds)):
     #print(preds[i])
     #plot_image(imgs[i], preds[i])
-    plot_images(i, f"Input {i}")
+    plot_images(i)
+    get_iou(i)
+    plot_iou(i)
 
 # print("Reality")
-# for i in range(len(preds) - 1):
+# for i in range(len(preds)):
 #     print(annotations[i])
 #     plot_image(imgs[i], annotations[i])
