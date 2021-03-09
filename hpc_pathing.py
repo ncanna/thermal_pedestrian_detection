1a70b67f0e8a1310f14eb846bde7e60616727c95
ncanna <na3au@virginia.edu>
2021-03-09 13:42:16 -0500

    Add faster-rcnn code

--- hpc_pathing.py
+++ hpc_pathing.py
@@ -1,7 +1,7 @@
 import os, time
 
-# Need to double check what the main folder is on rivanna
+# Check what the main folder is on rivanna
 main_folder = os.path.dirname(os.path.abspath(__file__))
-# time.sleep(5)
+time.sleep(5)
 print(main_folder)
-# os.makedirs("/scratch/sa3ag/modelRuns") #Check I can make a file in scratch from project
+# os.makedirs("/scratch/na3au/modelRuns")
